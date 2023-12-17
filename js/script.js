b270b93be4db0229e8c315fece4c9c7aa3323d86
Teddy <tedjus666@gmail.com>
2023-12-17 17:45:28 +0800

    added restart button for the ball

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+//Smooth Scroll
+
 //Bouncing Ball
 var ball = document.querySelector('.ball'),
     info = document.querySelector('.info'),
     y = 1,
     gravity = 1;
-    var intervalId = setInterval(function () {
+var intervalId = setInterval(function (bounce) {
         if (y >= 352) {
             clearInterval(intervalId);
         } else if (y > 350) {
@@ -14,4 +16,13 @@ var ball = document.querySelector('.ball'),
         y += gravity;
         ball.style.top = y + 'px';
         info.innerText = "Y : " + y + ", G : " + gravity;
-    }, 50);
+    }, 45);
+
+// Restart Ball Bounce
+const restartButton = document.getElementById('rball');
+
+restartButton.addEventListener('click', function () {
+    y = 0;
+    gravity = 0;
+    bounce();
+});
