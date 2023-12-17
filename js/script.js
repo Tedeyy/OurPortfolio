73ae179c9584bfa0ee46d9270e23588bb93089da
Teddy <tedjus666@gmail.com>
2023-12-17 19:02:46 +0800

    added contacts

--- js/script.js
+++ js/script.js
@@ -26,3 +26,16 @@ restartButton.addEventListener('click', function () {
     gravity = 0;
     bounce();
 });
+
+// Prompt
+function numberrobert() {
+    alert("Contact me, 090107306660");
+}
+
+function emailrobert() {
+    alert("Send me an email, robertpasukin042518@gmail.com");
+}
+
+function emailteddy() {
+    alert("Send me an email, 20221300@nbsc.edu.ph");
+}
